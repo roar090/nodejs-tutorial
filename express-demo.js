d3c2bb9af0026e8ac04cf643c56d5e8afb1ad917
roar090 <roar090@outlook.com>
2019-08-14 16:54:43 +0800

    update

--- express-demo.js
+++ express-demo.js
@@ -1,25 +1,131 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+//文件上传用到的包
+var multer  = require('multer');
 
-app.get('/', (req, res) => {
-    res.send('hello!');
-});
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//cookie处理的包
+var cookieParser = require('cookie-parser');
+var util = require('util');
+
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
+app.use(cookieParser());
+
 app.get('/index.html', (req, res) => {
-    var path = require('path');
-    res.sendFile(__dirname + '/index.html', (err) => {
+    res.sendFile(__dirname + '/' + 'index.html', (err) => {
         if (err) {
             console.log(err);
         }
     });
 });
 
+app.get('/process_get', function (req, res) {
+
+    // 输出 JSON 格式
+    var response = {
+        "first_name": req.query.first_name,
+        "last_name": req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+    // 输出 JSON 格式
+    var response = {
+        "first_name": req.body.first_name,
+        "last_name": req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+app.post('/file_upload', function (req, res) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + "/upload/" + req.files[0].originalname;
+    fs.readFile(req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.files[0].originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+});
+
+app.get('/cookie', function(req, res) {
+    console.log("Cookies: " + util.inspect(req.cookies));
+    res.redirect('/index.html');
+})
+
+app.get('/banner', (req, res) => {
+    res.sendFile(__dirname + '/banner.txt', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+//更多示例：
+//  主页输出 "Hello World"
+app.get('/', function (req, res) {
+    console.log("主页 GET 请求");
+    res.send('Hello GET');
+})
+
+
+//  POST 请求
+app.post('/', function (req, res) {
+    console.log("主页 POST 请求");
+    res.send('Hello POST');
+})
+
+//  /del_user 页面响应
+app.get('/del_user', function (req, res) {
+    console.log("/del_user 响应 DELETE 请求");
+    res.send('删除页面');
+})
+
+//  /list_user 页面 GET 请求
+app.get('/list_user', function (req, res) {
+    console.log("/list_user GET 请求");
+    res.send('用户列表页面');
+})
+
+// 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
+app.get('/ab*cd', function (req, res) {
+    console.log("/ab*cd GET 请求");
+    res.send('正则匹配');
+})
+
+function banner() {
+    var banner = fs.readFileSync('banner.txt');
+    console.log(banner.toString());
+}
+
+
 
 var server = app.listen(8888, () => {
+    banner();
     var host = server.address().address;
     var port = server.address().port;
-
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 });
+
 
 /**
  * Request 对象 - request 对象表示 HTTP 请求，
